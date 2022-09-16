45f7f6f8adca95429403b68469a646d94725e697
deniandriancode <deniandriancode@gmail.com>
2022-09-16 19:32:53 +0700

    python demo

--- main.py
+++ main.py
@@ -1,1 +1,4 @@
-print("Python in github action")
+with open("file.txt", "w") as fp:
+    fp.write("Dolor quas magnam minus magni dignissimos veniam Eveniet esse blanditiis tempora reprehenderit a? Asperiores reprehenderit in amet non pariatur, nulla. Natus illum beatae molestiae asperiores possimus? Cumque deserunt beatae aliquid. ")
+
+print("finish")
